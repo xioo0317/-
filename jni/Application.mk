18643d905119e1738fb2bd4d0d11806f28412149
xioo0317 <2977401730@qq.com>
2026-09-14 23:21:27 +0800

    fix: use c++_static to avoid missing libc++_shared.so at runtime

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,5 @@
 # Application.mk - Application-level build configuration
-APP_STL := c++_shared
+APP_STL := c++_static
 APP_CPPFLAGS := -std=c++17 -frtti -fexceptions
 APP_ABI := arm64-v8a
 APP_PLATFORM := android-24
